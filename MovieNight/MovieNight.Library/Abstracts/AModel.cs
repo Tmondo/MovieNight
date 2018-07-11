33db3ebcb53ec6f9286f23c17705cfa28acf4faa
Todd Tuttoilmondo <todd.tuttoilmondo@gmail.com>
2018-07-11 15:10:16 -0400

    add models and abstracts

---
+++ MovieNight/MovieNight.Library/Abstracts/AModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieNight.Library.Abstracts
+{
+
+
+    public class AModel
+    {
+        public Guid Id
+        {
+            get;
+        }
+
+        public AModel()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
